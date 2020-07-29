81ffecfc235963de7238d42c38e74f0589997503
Deco Moraes <deco.moraes@icloud.com>
2020-07-28 21:05:29 -0300

    userlogin

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hotels, Hotel
-from resources.user import Users, User, UserRegister
+from resources.user import Users, User, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 api = Api(app)
+jwt = JWTManager(app)
 
 @app.before_first_request
 def cria_banco():
@@ -17,6 +20,7 @@ api.add_resource(Hotel, '/hotel/<string:hotel_id>')
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from sql_alchemy import base
--- resources/user.py
+++ resources/user.py
@@ -1,10 +1,15 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
 
+atributos = reqparse.RequestParser()
+atributos.add_argument('username', type = str, required = True, help = 'You need  to insert your username')
+atributos.add_argument('password', type = str, required = True, help = 'You need  to insert your password')
 
 class Users(Resource):
 
-    
+
     def get(self):
         return {'Todos os Usuários': [user.json() for user in UserModel.query.all()]}
 
@@ -36,9 +41,6 @@ class UserRegister(Resource):
 
 
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('username', type = str, required = True, help = 'You need  to insert your username')
-        atributos.add_argument('password', type = str, required = True, help = 'You need  to insert your password')
         dados = atributos.parse_args()
 
         if UserModel.find_username(dados['username']):
@@ -47,3 +49,17 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return {'message': 'The user created succesfully'}, 201 # Created
+
+
+class UserLogin(Resource):
+
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_user(dados['username'])
+
+        if user and safe_str_cmp(user.password, dados['password']):
+            token_de_acesso = create_access_token(identity = user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {"message": "The username or password isn't correct"}, 401 # Unauthorized
